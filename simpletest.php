89d83513c0d23681407c43b0309f41a87df17cfb
angga0310 <anggabarry87@gmail.com>
2023-10-19 11:48:55 +0700

    upload2

--- simpletest.php
+++ simpletest.php
@@ -1,11 +1,16 @@
 <?php
+    //Menggunakan PHPUnit
     use PHPUnit\Framework\TestCase;
+
+    //membutuhkan wordcount.php
     require_once "wordcount.php";
     class Simpletest extends TestCase {
         public function testcountword (){
+            // Menggunakan Class yang akan di test / mendeklarasi class
             $wc = new wordcount();
 
-            $TestSentence = "My name is joko";
+            //Masukkan kata kata 
+            $TestSentence = "Dania Angga Barry Lana";
             $Wordcount = $wc ->countword($TestSentence);
             
             $this -> assertEquals(4, $Wordcount);
